5a69f858cc2e1e0a2b0f70e9b1cbdc1da8e2e9b8
NguyenTienDat <luanoutplay@gmail.com>
2024-02-22 16:53:27 +0700

    Remove unused code and update file paths

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
-import usersRouter from './../routes/users.routes'
-import databaseService from './../services/database.services'
+import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 const app = express()
 const port = 3000
 
